8a7daa8611fe34660878a09c12e36c65012e01b8
James Lee <babun5209@gmail.com>
2016-12-02 11:52:35 -0800

    Add initial views of a website and app.rb

---
+++ app.rb
@@ -0,0 +1,13 @@
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/word')
+require('./lib/definition')
+
+get('/') do
+  erb(:index)
+end
+
+get('/words/new') do
+  erb(:words_form)
+end
